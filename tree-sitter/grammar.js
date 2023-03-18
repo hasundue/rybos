e7428ff3e4b6b80e7a0e7db1c34cd67620feeabb
hasundue <hasundue@gmail.com>
2023-03-18 14:40:30 +0900

    Add binary operators and expressions

--- tree-sitter/grammar.js
+++ tree-sitter/grammar.js
@@ -7,10 +7,34 @@ module.exports = grammar({
     _expression: $ => choice(
       $.identifier,
       $.number,
+      $.binary_expression,
     ),
 
-    identifier: $ => /[a-zA-Z]+/,
+    identifier: $ => /[a-zA-Z_]+/,
 
     number: $ => /\d+/,
+
+    binary_expression: $ => choice(
+      $._binary_operator_prior,
+      $._binary_operator_post,
+    ),
+
+    _binary_operator_prior: $ => choice(
+      '*',
+      '/',
+    ),
+
+    _binary_expression_prior: $ => choice(
+      prec.left(2, seq($._expression, $._binary_operator_prior, $._expression)),
+    ),
+
+    _binary_operator_post: $ => choice(
+      '+',
+      '-',
+    ),
+
+    _binary_expression_post: $ => choice(
+      prec.left(1, seq($._expression, $._binary_operator_post, $._expression)),
+    ),
   },
 });
