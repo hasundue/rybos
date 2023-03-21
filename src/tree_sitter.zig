24db54526cfa7411cc0426cf9b1f70f308fc4d3e
hasundue <hasundue@gmail.com>
2023-03-21 12:31:13 +0900

    Abondan tree-sitter-rybos.zig

--- src/tree_sitter.zig
+++ src/tree_sitter.zig
@@ -3,38 +3,26 @@ const testing = std.testing;
 
 const c = @cImport({
     @cInclude("tree_sitter/api.h");
-    @cInclude("tree_sitter/parser.h");
 });
 
-const tree_sitter_rybos = @import("tree_sitter_rybos.zig");
-
-pub const Language = c.TSLanguage;
+extern "c" fn tree_sitter_rybos() *c.TSLanguage;
 
 pub const Parser = struct {
     c: ?*c.TSParser,
 
     pub fn init() Parser {
-        return .{
+        const parser = Parser{
             .c = c.ts_parser_new(),
         };
-    }
-
-    pub fn setLanguage(self: Parser, lang: Language) void {
-        c.ts_parser_set_language(self.c, lang);
-    }
-
-    pub fn language(self: Parser) Language {
-        return c.ts_parser_language(self.c);
+        _ = c.ts_parser_set_language(
+            parser.c,
+            tree_sitter_rybos(),
+        );
+        return parser;
     }
 };
 
 test "instantiate a parser object" {
     const parser = Parser.init();
     try testing.expectEqual(Parser, @TypeOf(parser));
-}
-
-test "set a language" {
-    const parser = Parser.init();
-    const rybos = tree_sitter_rybos.create();
-    parser.setLanguage(rybos);
 }
